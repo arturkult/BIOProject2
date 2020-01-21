00297851bb2422cf1f583b333d6b3348f647eaa7
Artur Różyński <a.rozynski@npc.pl>
2020-01-21 10:21:52 +0100

    add second fasta and some other commands

--- source.r
+++ source.r
@@ -1,3 +1,12 @@
-a <- DNAStringSet(DNAString("GGTACCAAATAGAA"))
-a <- append(a, "GGCACCAAACAGAA")
-consensus(read.alignment("plik1.fasta", format="fasta"), method = "threshold")
+library("seqinr")
+library("DECIPHER")
+
+consensus(read.alignment("plik1.fasta", format="fasta"), method = "profile")
+
+file1 <- readDNAStringSet("plik1.fasta")
+file2 <- readDNAStringSet("plik2.fasta")
+consensusMatrix(file1)
+#letterFrequency(file1, letters=c("A","C","T","G","-"))
+AlignProfiles(file1, file2, perfectMatch = 1, misMatch = -1, gapExtension = 0, gapOpening = 0)
+ConsensusSequence(file1)
+BrowseSeqs(file1)
