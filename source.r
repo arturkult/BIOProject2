f4c5e044d232680d638926a22bd292f9ec206a86
Adam Wasilewski <brzoza10k@gmail.com>
2020-01-22 21:54:18 +0100

    Add aligment with upgma tree

--- source.r
+++ source.r
@@ -10,3 +10,10 @@ consensusMatrix(file1)
 AlignProfiles(file1, file2, perfectMatch = 1, misMatch = -1, gapExtension = 0, gapOpening = 0)
 ConsensusSequence(file1)
 BrowseSeqs(file1)
+
+mergedFiles <- c(file1, file2)
+distanceMatrix <- DistanceMatrix(mergedFiles, includeTerminalGaps = FALSE,
+                                 penalizeGapLetterMatches = FALSE, penalizeGapGapMatches = FALSE)
+upgmaGuideTree <- IdClusters(distanceMatrix, method = "UPGMA", showPlot = TRUE, type = "dendrogram")
+AlignSeqs(mergedFilesWithoutGaps, guideTree = upgmaGuideTree)
+
